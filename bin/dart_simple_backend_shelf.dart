3cd1a9b4c3b9efbb103202746adc79f3d701c115
Jean Lucca Almeida Condratiuk <jluccacondratiuk@gmail.com>
2022-07-20 20:56:06 -0300

    Initial Commit

---
+++ bin/dart_simple_backend_shelf.dart
@@ -0,0 +1,5 @@
+import 'package:dart_simple_backend_shelf/dart_simple_backend_shelf.dart' as dart_simple_backend_shelf;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_simple_backend_shelf.calculate()}!');
+}
