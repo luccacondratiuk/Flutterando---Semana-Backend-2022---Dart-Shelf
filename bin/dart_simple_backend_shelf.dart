e6472b50cc499992f08bcce7b5fdb8c299d934a2
Jean Lucca Almeida Condratiuk <jluccacondratiuk@gmail.com>
2022-07-20 21:08:23 -0300

    Aula 3: Instalando e inciando Shelf Web Server

--- bin/dart_simple_backend_shelf.dart
+++ bin/dart_simple_backend_shelf.dart
@@ -1,5 +1,17 @@
-import 'package:dart_simple_backend_shelf/dart_simple_backend_shelf.dart' as dart_simple_backend_shelf;
+import 'dart:async';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_simple_backend_shelf.calculate()}!');
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:dart_simple_backend_shelf/dart_simple_backend_shelf.dart'
+    as dart_simple_backend_shelf;
+
+void main(List<String> arguments) async {
+  final server = await io.serve(_handler, '0.0.0.0', 4000);
+
+  print("Online em ${server.address.address}:${server.port}");
+}
+
+FutureOr<Response> _handler(Request request) {
+  print(request);
+  return Response(200, body: "Corpo");
 }
