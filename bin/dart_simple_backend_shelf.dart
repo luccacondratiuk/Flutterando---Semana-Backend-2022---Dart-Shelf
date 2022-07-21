7403e8581cbcac93a2a56e9a182bb0b8920269b3
Jean Lucca Almeida Condratiuk <jluccacondratiuk@gmail.com>
2022-07-20 21:21:05 -0300

    Aula 4: Handler - Middleware

--- bin/dart_simple_backend_shelf.dart
+++ bin/dart_simple_backend_shelf.dart
@@ -2,13 +2,26 @@ import 'dart:async';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
-import 'package:dart_simple_backend_shelf/dart_simple_backend_shelf.dart'
-    as dart_simple_backend_shelf;
 
 void main(List<String> arguments) async {
-  final server = await io.serve(_handler, '0.0.0.0', 4000);
+  var pipeline = Pipeline().addMiddleware(log());
+  final server = await io.serve(pipeline.addHandler(_handler), '0.0.0.0', 4000);
 
   print("Online em ${server.address.address}:${server.port}");
+}
+
+Middleware log() {
+  return (handler) {
+    return (request) async {
+      //* Antes de executar
+
+      print('Solicitação: ${request.url}');
+      var response = await handler(request);
+      //* Depois de executar
+      print('[${response.statusCode}] - Response Code');
+      return response;
+    };
+  };
 }
 
 FutureOr<Response> _handler(Request request) {
