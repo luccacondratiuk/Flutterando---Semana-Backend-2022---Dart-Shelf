ceb7e82fc37d3ac5a52fe4ab313a75ae1b2e7a08
Jean Lucca Almeida Condratiuk <jluccacondratiuk@gmail.com>
2022-07-21 23:47:17 -0300

    Aula 9: Conector do Postgres em Dart

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,5 +1,6 @@
-import 'dart:html';
-
+import 'package:dart_simple_backend_shelf/src/core/services/database/postgres/postgres_database.dart';
+import 'package:dart_simple_backend_shelf/src/core/services/database/remote_database.dart';
+import 'package:dart_simple_backend_shelf/src/core/services/dot_env/dot_env_service.dart';
 import 'package:dart_simple_backend_shelf/src/modules/user/user_resource.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_modular/shelf_modular.dart';
@@ -9,5 +10,11 @@ class AppModule extends Module {
   List<ModularRoute> get routes => [
         Route.get('/', (Request request) => Response.ok('Inicial')),
         Route.resource(UserResource()),
+      ];
+
+  @override
+  List<Bind> get binds => [
+        Bind.instance<DotEnvService>(DotEnvService.instance),
+        Bind.singleton<RemoteDatabase>((i) => PostgresDatabase(i())),
       ];
 }
---
+++ lib/src/core/services/database/postgres/postgres_database.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:dart_simple_backend_shelf/src/core/services/database/remote_database.dart';
+import 'package:dart_simple_backend_shelf/src/core/services/dot_env/dot_env_service.dart';
+import 'package:postgres/postgres.dart';
+import 'package:shelf_modular/shelf_modular.dart';
+
+class PostgresDatabase implements RemoteDatabase, Disposable {
+  final completer = Completer<PostgreSQLConnection>();
+  final DotEnvService dot_env;
+
+  PostgresDatabase(this.dot_env) {
+    _init();
+  }
+
+  _init() async {
+    final url = dot_env['DATABASE_URL']!;
+    final uri = Uri.parse(url);
+    var connection = PostgreSQLConnection(
+      uri.host,
+      uri.port,
+      uri.pathSegments.first,
+      username: uri.userInfo.split(":").first,
+      password: uri.userInfo.split(":").last,
+    );
+    await connection.open();
+    completer.complete(connection);
+  }
+
+  @override
+  Future<List<Map<String, Map<String, dynamic>>>> query(String query_text,
+      {Map<String, String> variables = const {}}) async {
+    final connection = await completer.future;
+    return connection.mappedResultsQuery(query_text,
+        substitutionValues: variables);
+  }
+
+  @override
+  void dispose() async {
+    final connection = await completer.future;
+    await connection.close();
+  }
+}
