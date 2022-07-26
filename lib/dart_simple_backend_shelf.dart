6101817ba5953b445c662b02bb68fa93ee285bba
Jean Lucca Almeida Condratiuk <jluccacondratiuk@gmail.com>
2022-07-26 19:14:46 -0300

    Aula 12: Middleware para content-type

--- lib/dart_simple_backend_shelf.dart
+++ lib/dart_simple_backend_shelf.dart
@@ -7,6 +7,22 @@ Future<Handler> startShelfModular() async {
 
   final handler = Modular(module: AppModule(), middlewares: [
     logRequests(),
+    jsonResponse(),
   ]);
   return handler;
+}
+
+Middleware jsonResponse() {
+  return (handler) {
+    return (request) async {
+      var response = await handler(request);
+      response = response.change(
+        headers: {
+          'content-type': 'application/json',
+          ...response.headers,
+        },
+      );
+      return response;
+    };
+  };
 }
